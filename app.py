c3294e9e9a14778be13179ef61b18fca5434e318
Ovlic <74332950+Ovlic@users.noreply.github.com>
2023-03-20 14:08:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ def unauthorized():
 
 # Naive database setup
 try:
-    init_db_command()
+    pass
+    #init_db_command()
 except sqlite3.OperationalError:
     # Assume it's already been created
     pass
