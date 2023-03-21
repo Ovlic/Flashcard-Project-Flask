b4f7e03ccd5418e055a6ce70668667ed6e78bcfa
Ovlic <74332950+Ovlic@users.noreply.github.com>
2023-03-21 15:55:17 -0700

    Update user.py

--- user.py
+++ user.py
@@ -1,6 +1,11 @@
 from flask_login import UserMixin
+import sqlite3
 
-from db import get_db
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 
 class User(UserMixin):
@@ -12,10 +17,11 @@ class User(UserMixin):
 
     @staticmethod
     def get(user_id):
-        db = get_db()
+        db = get_db_connection()
         user = db.execute(
             "SELECT * FROM user WHERE id = ?", (user_id,)
         ).fetchone()
+        db.close()
         if not user:
             return None
 
@@ -26,10 +32,11 @@ class User(UserMixin):
 
     @staticmethod
     def create(id_, name, email, profile_pic):
-        db = get_db()
+        db = get_db_connection()
         db.execute(
             "INSERT INTO user (id, name, email, profile_pic)"
             " VALUES (?, ?, ?, ?)",
             (id_, name, email, profile_pic),
         )
         db.commit()
+        db.close()
